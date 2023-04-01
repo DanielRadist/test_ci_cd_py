31fed31522f62d55f69d2542667dc2c71c15597c
DanielRadist <55454240+DanielRadist@users.noreply.github.com>
2023-04-01 11:11:51 +0700

    Привет

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello World!'
+    return 'Hello World! Привет!'
 
 def main():
     app.run(host='0.0.0.0', port=8080)
